bdb829ea603418ec89fe999a5ef0647bec88dcb3
Simonas Karuzas <simonas@not.cat>
2024-12-23 10:11:12 +0200

    feat: new backend endpoint

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'https://graphql.intuition-api.com/v1/graphql',
+  schema: 'https://dev.base.intuition-api.com/v1/graphql',
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ['src/**/*.{ts,tsx}'],
   generates: {
