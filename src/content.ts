0e83e436c0b7cd0a3299550483588e6376004424
Simonas Karuzas <simonas@not.cat>
2025-01-10 14:58:02 +0200

    feat: use metamask-extension-provider

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,2 @@
-// Inject the script to interact with the Ethereum wallet
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('dist/in-page.js');
-(document.head || document.documentElement).appendChild(script);
 
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === "FROM_POPUP") {
-    window.postMessage(request, "*");
-  }
-});
-
-// Listen for messages from the injected script
-window.addEventListener("message", (event) => {
-  if (event.data.type === "FROM_IFRAME") {
-    chrome.runtime.sendMessage(event.data);
-  }
-});
 
